7fc4e3d71728a3aa09f1790ebea7207dee4b69bc
Juan Manuel Lora <cyselectronica@hotmail.com>
2013-06-09 08:38:37 -0500

    cambios en la interfaz grafica

--- assets/app/main.js
+++ assets/app/main.js
@@ -4,3 +4,10 @@ require(['backbone', 'router'], function (Backbone, Router) {
 	var router = new Router();
 	Backbone.history.start();
 });
+
+$('#reportsBtn').bind('click', 
+      function(e){
+          alert('mostrar');
+
+      } 
+);
