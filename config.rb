5bc4f5bc7d5e5c984e99cd2e9d8980f8f3149b26
mastersanto <jorgeivansandoval@gmail.com>
2013-08-13 00:36:10 -0700

    Markup and Styles

--- config.rb
+++ config.rb
@@ -3,5 +3,6 @@ http_images_path = "../img"
 css_dir = "assets/css"
 sass_dir = "assets/css/src"
 images_dir = "assets/img"
+http_generated_images_path = "../img"
 
 line_comments=false
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('./api')(server);
 server.configure(function() {
 	server.set('view engine', 'ejs');
 	server.use('/css', express.static(__dirname + '/assets/css'));
+	server.use('/img', express.static(__dirname + '/assets/img'));
 	server.use('/app', express.static(__dirname + '/assets/app'));
 	server.use('/scripts', express.static(__dirname + '/components'));
 });
