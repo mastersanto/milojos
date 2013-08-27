ce726c2cff832122e9d765719810fcd9a83b1ce3
mastersanto <jorgeivansandoval@gmail.com>
2013-08-27 00:23:22 -0700

    Toogle Filters/Settings sidebar

--- assets/app/views/header/header.js
+++ assets/app/views/header/header.js
@@ -11,7 +11,22 @@ define([
   ) {
 
   return Marionette.ItemView.extend({
-    template: tpl('header/templates/header.html')
+    template: tpl('header/templates/header.html'),
+    events: {
+      'touchstart #filters-btn' : 'toogleFilters',
+      'touchstart #settings-btn' : 'toogleSettings'
+    },
+    toogleFilters: function() {
+      $('#wrapper').toggleClass('to-right');
+      $('#filters-wrapper').toggleClass('active');
+    },
+    toogleSettings: function() {
+      $('#wrapper').toggleClass('to-left');
+      $('#settings-wrapper').toggleClass('active');
+    },
+	  onRender: function(){
+	  	// el
+	  }
   });
 
 });
